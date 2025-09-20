27f59025506cb04e8ae943b9a1828031319aa061
Estefanía <47468069+tefaceli@users.noreply.github.com>
2025-09-20 13:33:00 -0500

    update

--- guayasss/config.js
+++ guayasss/config.js
@@ -12,11 +12,9 @@ var config = {
     projection: 'mercator',
     use3dTerrain: false,
     auto: false,
-    footer: `<p><strong>Metodología:</strong> Para la elaboración de este mapa, PRIMICIAS utilizó la base de datos de muertes violentas del Ministerio del Interior, disponible en el portal de Datos Abiertos, que incluye las coordenadas de cada suceso. Con esas ubicaciones, cada muerte violenta fue ubicada en un mapa.
-            </p>
-            <p>De acuerdo a la ubicación de cada punto, estos fueron asignados a las parroquias y las zonas usando la biblioteca de código abierto de Python GeoPandas, que permite trabajar con datos espaciales. PRIMICIAS utilizó la delimitación de zonas establecida por el Consejo Nacional Electoral para las elecciones, por ser la única aproximación disponible a los barrios o sectores de los cantones en Ecuador.
+    footer: `<p><strong>Metodología:</strong> Para este mapa, PRIMICIAS utilizó la base de datos de muertes violentas del Ministerio del Interior. Con la geolocalización de cada crimen se ubicó un punto siguiendo la delimitación por parroquias y zonas del Consejo Nacional Electoral. Para ello, se usó la biblioteca de código abierto de Python GeoPandas. El censo de población 2022 del INEC no tiene datos de habitantes por parroquias urbanas, por lo que no se pudo levantar la tasa de homicidios.
             <p>
-            <p>Análisis de datos, redacción y desarrollo por Estefanía Celi.</p>`,
+            <p>Análisis de datos, redacción y desarrollo por Estefanía Celi | PRIMICIAS</p>`,
     chapters: [
         {   id: 'first-chapter',
             alignment: 'center',
@@ -56,9 +54,9 @@ var config = {
             description: `
                 <p>A escala nacional, Guayas es donde sus habitantes sienten la violencia más cercana, pues se vive a diario.
                 </p>
-                <p>En esa provincia, este 2025 se registraron dos picos de muertes violentas, dos días en los que hubo 31 muertos: el 6 de marzo y el 2 de mayo. En esas dos fechas, se registraron <a href="https://www.primicias.ec/seguridad/ecuador-masacres-violencia-multiple-victimas-102740/">dos grandes masacres en Guayaquil</a>.
+                <p>En esa provincia, en el mes de agosto se registró el peor pico de violencia de este año: el 10 de agosto hubo 33 muertos en esa provincia. Ese día hubo <a href="https://www.primicias.ec/sucesos/nueva-masacre-discoteca-santa-lucia-asesinato-guayas-102682/">una matanza en Santa Lucía</a> y otra en <a href="https://www.primicias.ec/sucesos/asesinados-monte-sinai-muertes-violentas-guayaquil-ecuador-102702/">en Monte Sinaí</a>.
                 </p>
-                <p>En promedio, 12 personas han muerto diariamente en Guayas en los primeros siete meses de 2025.
+                <p>En promedio, 11 personas han muerto diariamente en Guayas en los primeros ocho meses de 2025.
                 </p>`
                 ,
             location: {
@@ -101,7 +99,7 @@ var config = {
             hidden: false,
             title: 'Guayaquil',
             //image: '',
-            description: `<p>El cantón Guayaquil, la capital de Guayas y la ciudad más poblada de Ecuador, concentra el 30% de los asesinatos a escala nacional entre enero y julio de 2025. Allí, el promedio es el más alto del país: siete víctimas al día o una cada 3 horas con 10 minutos.
+            description: `<p>El cantón Guayaquil, la capital de Guayas y la ciudad más poblada de Ecuador, concentra el 29% de los asesinatos a escala nacional entre enero y agosto de 2025. Allí, el promedio es el más alto del país: siete víctimas al día o una cada 3 horas y 14 minutos.
             </p>
             <h4 class="chart-title">Días con al menos una muerte violenta en las parroquias de Guayaquil</h4>
             <iframe title="Días con al menos una muerte violenta en las parroquias de Guayaquil" aria-label="Gráfico de barras" id="datawrapper-chart-heUnq" src="https://datawrapper.dwcdn.net/heUnq/4/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="168" data-external="1"></iframe><script type="text/javascript">!function(){"use strict";window.addEventListener("message",function(a){if(void 0!==a.data["datawrapper-height"]){var e=document.querySelectorAll("iframe");for(var t in a.data["datawrapper-height"])for(var r,i=0;r=e[i];i++)if(r.contentWindow===a.source){var d=a.data["datawrapper-height"][t]+"px";r.style.height=d}}})}();
@@ -145,7 +143,7 @@ var config = {
             hidden: false,
             title: 'Pascuales',
             //image: '',
-            description: `<p>Pascuales, en el norte de Guayaquil, es la parroquia más afectada por la violencia a escala nacional. Entre enero y julio de 2025, suma 555 homicidios: que significan el 10% de todas las muertes violentas en el país. Esta parroquia es también una de las más densamente pobladas de la ciudad, aunque su número exacto de habitantes no consta en <a href="https://cubos.inec.gob.ec/AppCensoEcuador/">los resultados del censo 2022</a>.
+            description: `<p>Pascuales, en el norte de Guayaquil, es la parroquia más afectada por la violencia a escala nacional. Entre enero y agosto de 2025, suma 610 homicidios: que significan el 10% de todas las muertes violentas en el país. Esta parroquia es también una de las más densamente pobladas de la ciudad, aunque su número exacto de habitantes no consta en <a href="https://cubos.inec.gob.ec/AppCensoEcuador/">los resultados del censo 2022</a>.
             </p>
             <p>Pascuales llegó a tener 21 días seguidos con asesinatos, entre el 21 de febrero y el 3 de marzo.
             </p>
@@ -189,9 +187,9 @@ var config = {
             hidden: false,
             title: 'Paraíso de la Flor',
             //image: '',
-            description: `<p>Paraíso de la Flor, uno de los barrios de Pascuales, tiene las peores cifras del año. En los primeros siete meses de 2025, hubo 79 muertes violentas, lo que lo convierte en el barrio más peligroso de Guayaquil, sólo por debajo del centro de Pascuales (80).
+            description: `<p>Paraíso de la Flor, uno de los barrios de Pascuales, tiene las peores cifras del año. En los primeros ocho meses de 2025, hubo 84 muertes violentas, lo que lo convierte en el barrio más peligroso de Guayaquil.
             </p>
-            <p>En esta zona de la ciudad han habido 50 días con al menos un asesinato. Varios de ellos están concentrados a lo largo de la avenida 24-A, la arteria principal de este sector.
+            <p>En esta zona de la ciudad han habido 53 días con al menos un asesinato. Varios de ellos están concentrados a lo largo de la avenida 24-A, la arteria principal de este sector.
             </p>`,
             location: {
                 center: [-79.95760, -2.09571],
@@ -222,9 +220,9 @@ var config = {
             hidden: false,
             title: 'Tarqui',
             //image: '',
-            description: `<p>Tarqui, la parroquia más grande de Guayaquil, concentra en su área urbana también gran parte de muertes violentas: 397.
+            description: `<p>Tarqui, la parroquia más grande de Guayaquil, concentra en su área urbana también gran parte de muertes violentas: 444.
             </p>
-            <p>En esa parroquia llegó a haber 23 asesinatos en un sólo día: el 6 de marzo de 2025. En los siete meses de 2025, en Tarqui hay una muerte violenta cada 12 horas y 45 minutos en promedio.
+            <p>En esa parroquia llegó a haber 23 asesinatos en un sólo día: el 6 de marzo de 2025. En los ocho meses de 2025, en Tarqui hay una muerte violenta cada 13 horas y 4 minutos.
             </p>
             <p>Allí, se concentran también barrios de mucha peligrosidad, como Socio Vivienda</p>
             </script>`,
@@ -267,7 +265,7 @@ var config = {
             hidden: false,
             title: 'Ximena',
             //image: '',
-            description: `<p>Ximena, en el sur de Guayaquil, es la tercera parroquia con más muertes violentas entre enero y julio de 2025. Pero la diferencia es que en esta parroquia, la situación empeoró drásticamente en mayo, cuando se registraron 87 asesinatos en 26 días.
+            description: `<p>Ximena, en el sur de Guayaquil, es la tercera parroquia con más muertes violentas entre enero y agosto de 2025. Pero la diferencia es que en esta parroquia, la situación empeoró drásticamente en mayo, cuando se registraron 87 asesinatos en 26 días.
             </p>
             <p>En Ximena, el promedio es de una muerte violenta cada 15 horas. Y estos sucesos se concentran en barrios como Trinipuerto, Isla Trinitaria y Guasmo Norte.</p>
             </script>`,
@@ -310,7 +308,7 @@ var config = {
             hidden: false,
             title: 'Durán',
             //image: '',
-            description: `<p>Durán es el segundo cantón más violento de Guayas: en siete meses suma ya 370 muertes violentas. Es así que tiene en promedio una muerte violenta cada 13 horas y 40 minutos, más que el promedio de Tarqui, en Guayaquil.
+            description: `<p>Durán es el segundo cantón más violento de Guayas: en ocho meses suma ya 390 muertes violentas. Es así que tiene en promedio un asesinato cada 14 horas y 50 minutos, más que el promedio de Ximena, en Guayaquil.
             </p>
             <p>De sus tres parroquias, la más afectada ha sido Divino Niño.
             </p>
@@ -356,9 +354,9 @@ var config = {
             hidden: false,
             title: 'El sector Secap',
             //image: '',
-            description: `<p>Secap, que más que un barrio es un sector de la parroquia Divino Niño, de Durán, es el más afectado del cantón. Pero no sólo eso. Es la zona más violenta de todo el país en los primeros siete meses de 2025.
+            description: `<p>Secap, que más que un barrio es un sector de la parroquia Divino Niño, de Durán, es el más afectado del cantón. Pero no sólo eso. Es la zona más violenta de todo el país en los primeros ocho meses de 2025.
             </p>
-            <p>En el sector Secap, entre enero y julio de 2025, se han registrado 124 asesinatos, un número mayor al Paraíso de la Flor, Balerio Estacio o Socio Vivienda, en Guayaquil.
+            <p>En el sector Secap, entre enero y agosto de 2025, se han registrado 128 asesinatos, un número mayor al Paraíso de la Flor, Balerio Estacio o Socio Vivienda, en Guayaquil.
             </script>
             `,
             location: {
